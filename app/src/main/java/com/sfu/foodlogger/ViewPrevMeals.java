df2dcf1bf93878587bc668d15de0ccbf5447ca4c
Kenrick <kenricklam98@gmail.com>
2021-04-18 12:51:51 -0700

    Finished previous meal and help pages.

--- app/src/main/java/com/sfu/foodlogger/ViewPrevMeals.java
+++ app/src/main/java/com/sfu/foodlogger/ViewPrevMeals.java
@@ -3,6 +3,8 @@ package com.sfu.foodlogger;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,18 @@ public class ViewPrevMeals  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prev_meals_list);
+
+        Button prevMealsBackBtn = (Button) findViewById(R.id.prev_meal_back_btn);
+        btnPress(prevMealsBackBtn, LoggedInMenu.makeIntent(ViewPrevMeals.this));
+    }
+
+    private void btnPress(Button button, final Intent intent) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 
     public static Intent makeIntent(Context context) {
