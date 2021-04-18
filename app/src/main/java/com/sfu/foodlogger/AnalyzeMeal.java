40e3fbb1fee4c2b8f5ad7122fdcd65c9a08f9103
lkoo <lkoo@sfu.ca>
2021-04-18 14:30:47 -0700

    analyze meal page complete

--- app/src/main/java/com/sfu/foodlogger/AnalyzeMeal.java
+++ app/src/main/java/com/sfu/foodlogger/AnalyzeMeal.java
@@ -1,16 +1,170 @@
 package com.sfu.foodlogger;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.Context;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AnalyzeMeal extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
+    Button takeMealPhotoBtn;
+    Button analyzeMealPhotoBtn;
+    Button generateReportBtn;
+    ImageButton backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.analyze_meal);
+
+        backBtn = (ImageButton) findViewById(R.id.backBtn);
+        takeMealPhotoBtn = (Button) findViewById(R.id.take_meal_photo_btn);
+        analyzeMealPhotoBtn = (Button) findViewById(R.id.analyze_meal_photo_btn);
+        generateReportBtn = (Button) findViewById(R.id.generate_report_button);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        takeMealPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        analyzeMealPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                analyzeMealIngredients();
+            }
+        });
+
+        btnPress(generateReportBtn, MealReport.makeIntent(AnalyzeMeal.this));
+    }
+
+    private void btnPress(Button button, final Intent intent) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void analyzeMealIngredients() {
+        Spinner spinner1 = (Spinner) findViewById(R.id.ingredient1Unit);
+        Spinner spinner2 = (Spinner) findViewById(R.id.ingredient2Unit);
+        Spinner spinner3 = (Spinner) findViewById(R.id.ingredient3Unit);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ingredient_units, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner1.setAdapter(adapter);
+        spinner1.setSelection(0);
+        spinner2.setAdapter(adapter);
+        spinner2.setSelection(0);
+        spinner3.setAdapter(adapter);
+        spinner3.setSelection(8);
+
+        ScrollView ingredients = (ScrollView) findViewById(R.id.ingredientScrollView);
+        ingredients.setVisibility(View.VISIBLE);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.sfu.foodlogger.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            loadImageFromFile();
+        }
+    }
+
+    public void loadImageFromFile(){
+        ImageView view = (ImageView)this.findViewById(R.id.meal_image_view);
+        view.setVisibility(View.VISIBLE);
+
+        int targetW = view.getWidth();
+        int targetH = view.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        view.setImageBitmap(bitmap);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        analyzeMealPhotoBtn.setVisibility(View.VISIBLE);
+        return image;
     }
 
     public static Intent makeIntent(Context context) {
